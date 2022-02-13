17e74aba91979608dfc1586672a7ce1772632edc
timrusin <timrusin@gmail.com>
2022-02-13 17:13:52 -0600

    card models updated to work properly with spotify model

--- models/playlist.js
+++ models/playlist.js
@@ -4,17 +4,16 @@ const mongoose = require('../db/connection')
 // we want a schema with a title and a url 
 const PlaylistSchema = new mongoose.Schema({
     name: String,
-    title: String, // give it a name and a data type!
-    url: String,
-    category: [String],
+    description: String,
+    explicit: Boolean,
     images: [{
         height: Number,
         url: String,
         width: Number
     }],
-    creators: [String],
-    description: String
-
+    total_episodes: Number,
+    publisher: String,
+    external_urls: Object
 })
 
 const Playlist = mongoose.model('Playlist', PlaylistSchema)
