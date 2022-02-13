448bb75b48117400bd48e69061a2f57ac232229b
timrusin <timrusin@gmail.com>
2022-02-12 18:45:38 -0600

    updated playlist model to match spotify data

--- models/playlist.js
+++ models/playlist.js
@@ -1,24 +1,31 @@
 // require that mongoose package! 
 const mongoose = require('../db/connection')
 
-// we want a schema with a title and a url 
 const PlaylistSchema = new mongoose.Schema({
-    name: String,
-    title: String, // give it a name and a data type!
-    url: String,
-    category: [String],
+    available_markets: Array,
+    copyrights: Array,
+    description: String,
+    explicit: Boolean,
+    external_url: {
+        spotify: String
+    },
+    href: String,
+    html_description: String,
+    id: String,
     images: [{
         height: Number,
         url: String,
         width: Number
     }],
-    creators: [String],
-    description: String
-
+    is_externally_hosted: Boolean,
+    languages: Array,
+    media_type: String,
+    name: String,
+    publisher: String,
+    total_episodes: Number,
+    type: String,
+    uri: String
 })
 
 const Playlist = mongoose.model('Playlist', PlaylistSchema)
-
-// export it so other files can access it! 
-
 module.exports = Playlist
