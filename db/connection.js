3ecd1f0691f508233433085de950e4289ccb7715
tatcn8 <96024645+tatcn8@users.noreply.github.com>
2022-02-16 09:49:39 -0600

    deployment scripts completed; waiting to deploy

--- db/connection.js
+++ db/connection.js
@@ -10,7 +10,7 @@ const mongoURI = process.env.DATABASE_URL
 const db = mongoose.connection
 
 // Connect to Mongo
-mongoose.connect(mongoURI)
+mongoose.connect(process.env.MONGODB_URI || mongoURI) //THIS WAS CHANGED FOR DEPLOY
 
 // Connection Error/Success - optional but can be helpful
 // Define callback functions for various events
